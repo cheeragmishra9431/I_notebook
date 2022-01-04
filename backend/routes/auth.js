4bcfada6593a80862a0dede16eb88a49a4ce104e
cheeragmishra9431 <waytogocheerag@gmail.com>
2022-01-04 14:28:19 +0530

    Implemented bcrypt json with salt and implemented JWT token with secret signature

--- backend/routes/auth.js
+++ backend/routes/auth.js
@@ -2,8 +2,9 @@ const express=require('express');
 const router=express.Router();
 const User=require('../models/User');
 const { body, validationResult } = require('express-validator');
-
-
+const bcrypt = require('bcryptjs');
+const JWT_SECRET='Harryisagoodboy';
+const jwt=require('jsonwebtoken');
 
 //Create a user using :POST " /api/auth/". doesnt require auth. No login reequired
 router.post('/createuser',[body('name',"Enter a valid name").isLength({min:3}),body('email',"Enter a valid email").isEmail(),body('password', "Enter a password at least of 5 characters").isLength({ min: 5 }),
@@ -24,12 +25,19 @@ router.post('/createuser',[body('name',"Enter a valid name").isLength({min:3}),b
     if(user){
       return res.status(400).json({error: "Sorry a user with this email alreadt exists"})
     }
+    const salt=await bcrypt.genSalt(10);
+    secPass=await bcrypt.hash(req.body.password,salt);
     user= await User.create({
         name: req.body.name,
-        password: req.body.password,
+        password:secPass,
         email: req.body.email
       })
-      res.json({user});
+      const data={user:{
+        id:user.id
+      }}
+      const authtoken=jwt.sign(data,JWT_SECRET);
+      // console.log(authtoken); for debugging purposes
+      res.json({authtoken});
     }catch(error){
       console.log(error.message);
       res.status(500).send("some error occured");
