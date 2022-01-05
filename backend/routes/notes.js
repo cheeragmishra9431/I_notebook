01be37cd70011584805e4b8ff82ccf244755afd0
cheeragmishra9431 <waytogocheerag@gmail.com>
2022-01-05 22:24:38 +0530

    Made update end point using put request

--- backend/routes/notes.js
+++ backend/routes/notes.js
@@ -4,7 +4,7 @@ const fetchuser = require("../middleware/fetchuser");
 const Note = require("../models/Notes");
 const { body, validationResult } = require("express-validator");
 
-// ROUTE 3// Get all the notes "/api/auth/getuser". login required
+// ROUTE 1// Get all the notes "/api/notes/getuser". login required
 router.get("/fetchallnotes", fetchuser, async (req, res) => {
 
     try {
@@ -17,7 +17,7 @@ router.get("/fetchallnotes", fetchuser, async (req, res) => {
   
 });
 
-// ROUTE 3// add a new note  "/api/auth/addnote". login required
+// ROUTE 3// add a new note  "/api/notes/addnote". login required
 router.post(
   "/addnote",
   fetchuser,
@@ -47,4 +47,27 @@ router.post(
 
   }
 );
+// ROUTE 3// Update an existing note  "/api/notes/update/:id". login required    PUT request used note it.
+router.put(
+    "/update/:id",
+    fetchuser,async (req, res) => {
+        const {title, description, tag}=req.body;
+        //Create a new object
+        const newNote={};
+        if(title){newNote.title=title};
+        if(description){newNote.description=description};
+        if(tag){newNote.tag=tag};
+        //find the node to be updated
+        var note=await Note.findById(req.params.id); // remember to var not note as reassigning will happen later
+        if(!note){
+            return res.status(404).send("Not found")
+        }
+        if(note.user.toString()!=req.user.id){
+            return res.status(401).send("Not allowed")
+
+        }
+        note=await Note.findByIdAndUpdate(req.params.id, {$set:newNote}, {new:true})
+        res.json({note});
+    });
+
 module.exports = router;
