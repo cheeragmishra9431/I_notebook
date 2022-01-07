f6b47ea0035424996a973c4af794550d37787393
cheeragmishra9431 <waytogocheerag@gmail.com>
2022-01-07 22:38:45 +0530

    updated everything fetch notes functionality

--- backend/index.js
+++ backend/index.js
@@ -1,12 +1,16 @@
 const connectToMongo= require('./db');
 const express = require('express')
+var cors = require('cors')
 connectToMongo();
 
 const app = express()
 const port = 5000
 // available routes
+// var app = express()
 
+app.use(cors())
 app.use(express.json());
+
 app.get('/', (req, res) => {
   res.send('Hello World!')
 })
