c24b245ac59546aeea4b482e1f2e37cd52375e43
cheeragmishra9431 <waytogocheerag@gmail.com>
2022-01-10 21:53:33 +0530

    completed login using navigate and modified success in json of auth

--- backend/routes/auth.js
+++ backend/routes/auth.js
@@ -71,6 +71,7 @@ router.post(
   ],
   async (req, res) => {
     // If there are errors , return Bad requests and the errors
+    let success=false;
     const errors = validationResult(req);
     if (!errors.isEmpty()) {
       return res.status(400).json({ errors: errors.array() });
@@ -79,15 +80,17 @@ router.post(
     try {
       let user = await User.findOne({ email });
       if (!user) {
+        success=false;
         return res
           .status(400)
-          .json({ error: "Please try to login with correct credentials" });
+          .json({ success,error: "Please try to login with correct credentials" });
       }
       const passwordCompare = await bcrypt.compare(password, user.password);
       if (!passwordCompare) {
+        success=false;
         return res
           .status(400)
-          .json({ error: "Please try to login with correct credentials" });
+          .json({success, error: "Please try to login with correct credentials" });
       }
       const data = {
         user: {
@@ -95,7 +98,8 @@ router.post(
         },
       };
       const authtoken = jwt.sign(data, JWT_SECRET);
-      res.json({ authtoken });
+      success=true;
+      res.json({ success,authtoken });
     } catch (error) {
       console.log(error.message);
       res.status(500).send("Internal server error occured");
